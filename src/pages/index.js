46805c35c123081794e1d7bc2b3039dc97e9615b
HasbullahMnor <hasbullahmnor@gmail.com>
2018-07-26 12:08:09 +0800

    change write-up content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React apps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great. Just dont do it half way.</p>
+    <h1>Learning React</h1>
+    <p>Creating my wedding website by learning to design and code React apps</p>
+    <p>Lets go build something great. Just dont do it half way.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
