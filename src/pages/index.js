79d9fb3f370bc54448b1707f534f4996823bf58c
HasbullahMnor <hasbullahmnor@gmail.com>
2018-07-26 14:45:58 +0800

    removed default css, add new css, removed default header, added images folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learning React</h1>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learning React</h1>
     <p>Creating my wedding website by learning to design and code React apps</p>
     <p>Lets go build something great. Just dont do it half way.</p>
     <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
